cd8bdfb4f8085938e82b32ef1fb502087fb26293
aryandi <aryandi@netzme.id>
2016-07-01 13:18:33 +0700

    Tambah permission di Marshmallow

--- app/src/main/java/id/netzme/aryandi/getcontactname/MainActivity.java
+++ app/src/main/java/id/netzme/aryandi/getcontactname/MainActivity.java
@@ -1,25 +1,39 @@
 package id.netzme.aryandi.getcontactname;
 
+import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String EMAIL_SEPARATOR = "@";
     private static final String GOOGLE_ACCOUNT_IDENTIFIER = "com.google";
+    private static final int REQUEST_GET_ACCOUNT = 112;
 
     private AccountManager accountManager;
 
     private ContentResolver contentResolver;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +43,28 @@ public class MainActivity extends AppCompatActivity {
         accountManager = AccountManager.get(getApplicationContext());
         contentResolver = this.getContentResolver();
 
-        getFullNameFromGoogle();
+        textView = (TextView) findViewById(R.id.textNama);
+
+        if(android.os.Build.VERSION.SDK_INT > 22){
+            if(isGETACCOUNTSAllowed()){
+                // do your task
+
+//                getMailAddress();
+
+                String nama = getFullNameFromGoogle();
+
+                if (nama != null){
+                    textView.setText(nama);
+                }
+
+                return;
+            }else{
+                requestGET_ACCOUNTSPermission();
+            }
+
+        }
+
+
     }
 
     private Account[] getGoogleAccounts() {
@@ -43,10 +78,12 @@ public class MainActivity extends AppCompatActivity {
         if (accounts.length > 0) {
             Cursor cursor = queryEmailAccount(accounts);
 
-            try {
-                return findFullName(cursor, ContactsContract.Contacts.DISPLAY_NAME);
-            } finally {
-                cursor.close();
+            if (cursor != null) {
+                try {
+                    return findFullName(cursor, ContactsContract.Contacts.DISPLAY_NAME);
+                } finally {
+                    cursor.close();
+                }
             }
         }
 
@@ -56,12 +93,12 @@ public class MainActivity extends AppCompatActivity {
     private String findFullName(Cursor cursor, String fieldName) {
         int fullNameColumnIndex = cursor.getColumnIndex(fieldName);
 
-        while (cursor.moveToNext()) {
-            String fullName = cursor.getString(fullNameColumnIndex);
-            if (fullName != null && !fullName.isEmpty()) {
-                return fullName;
+            while (cursor.moveToNext()) {
+                String fullName = cursor.getString(fullNameColumnIndex);
+                if (fullName != null && !fullName.isEmpty()) {
+                    return fullName;
+                }
             }
-        }
 
         return null;
     }
@@ -83,11 +120,17 @@ public class MainActivity extends AppCompatActivity {
             accountNameList.add(accounts[index].name);
         }
 
+//        return contentResolver.query(
+//                ContactsContract.CommonDataKinds.Email.CONTENT_URI, new String[] { ContactsContract.CommonDataKinds.Email.CONTACT_ID,
+//                        ContactsContract.Data.DISPLAY_NAME },
+//                filterQuery.toString(),
+//                accountNameList.toArray(new String[length]), null);
+
         return contentResolver.query(
-                ContactsContract.CommonDataKinds.Email.CONTENT_URI, new String[] { ContactsContract.Contacts._ID,
-                        ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.HAS_PHONE_NUMBER },
+                ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
                 filterQuery.toString(),
                 accountNameList.toArray(new String[length]), null);
+
     }
 
     public String getGoogleUsername() {
@@ -106,6 +149,73 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return null;
+    }
+
+    private boolean isGETACCOUNTSAllowed() {
+        //Getting the permission status
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS);
+
+        //If permission is granted returning true
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        //If permission is not granted returning false
+        return false;
+    }
+
+
+    //if you don't have the permission then Requesting for permission
+    private void requestGET_ACCOUNTSPermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.GET_ACCOUNTS)){
+
+
+        }
+
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.GET_ACCOUNTS},REQUEST_GET_ACCOUNT);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if(requestCode == REQUEST_GET_ACCOUNT){
+
+            //If permission is granted
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                Toast.makeText(this,"Thanks You For Permission Granted ", Toast.LENGTH_LONG).show();
+
+//                getMailAddress();
+
+                String nama = getFullNameFromGoogle();
+
+                if (nama != null){
+                    textView.setText(nama);
+                }
+
+            }else{
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this,"Oops you just denied the permission",Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    public void getMailAddress(){
+
+        String possibleEmail = null;
+
+        Pattern emailPattern = Patterns.EMAIL_ADDRESS; // API level 8+
+        Account[] accounts = AccountManager.get(MainActivity.this).getAccountsByType(
+                GOOGLE_ACCOUNT_IDENTIFIER);
+        for (Account account : accounts) {
+            if (emailPattern.matcher(account.name).matches()) {
+                possibleEmail = account.name;
+                Log.i("MY_EMAIL_count", "" + possibleEmail);
+            }
+        }
     }
 
 }
